289a2a1767f7aa4c716868e6354055aa5429b1f1
VM <87504727+VanesaMoreno@users.noreply.github.com>
2025-08-11 22:15:32 -0300

    Create gestionDatos01.js
    Inicio del TP1 en Programcion3

---
+++ gestionDatos01.js
@@ -0,0 +1,41 @@
+
+let productos = [
+  { id: 1,
+    nombre: "Mouse", 
+    precio: 2500, 
+    stock: 10 },
+
+  { id: 2, 
+    nombre: "Teclado", 
+    precio: 4500, 
+    stock: 5 },
+
+  { id: 3, 
+    nombre: "Monitor", 
+    precio: 30000, 
+    stock: 2 },
+
+  { id: 4, 
+    nombre: "Notebook", 
+    precio: 150000, 
+    stock: 1 },
+  { id: 5, 
+    nombre: "Auriculares", 
+    precio: 3500, 
+    stock: 8 }
+];
+// 2.1
+// Imprimir la longitud del arrays productos
+//console.log('Cantidad total de productos :', productos.length);
+
+// 2.2
+//console.log([productos[1]]);
+//console.log([productos[3]]);
+
+//3.1 -3.2
+// for ( let producto of productos){
+//   console.log( `Nombre: ${producto.nombre}, Precio: $${producto.precio}`);
+//  };
+
+
+
